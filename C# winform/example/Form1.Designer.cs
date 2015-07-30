5b1c12ef75e508387423fd184bee38edc7e833e6
myunglark <myunglark96@naver.com>
2015-07-30 15:26:35 +0900

    last codes

---
+++ C# winform/example/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.메뉴ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.정보ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.timer6 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::example.Properties.Resources._2;
+            this.pictureBox2.Location = new System.Drawing.Point(473, 84);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(265, 327);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Enabled = false;
+            this.pictureBox1.Image = global::example.Properties.Resources._2;
+            this.pictureBox1.Location = new System.Drawing.Point(121, 84);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(265, 327);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.메뉴ToolStripMenuItem,
+            this.정보ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(828, 28);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // 메뉴ToolStripMenuItem
+            // 
+            this.메뉴ToolStripMenuItem.Name = "메뉴ToolStripMenuItem";
+            this.메뉴ToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.메뉴ToolStripMenuItem.Text = "메뉴";
+            // 
+            // 정보ToolStripMenuItem
+            // 
+            this.정보ToolStripMenuItem.Name = "정보ToolStripMenuItem";
+            this.정보ToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.정보ToolStripMenuItem.Text = "정보";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 20F);
+            this.label1.Location = new System.Drawing.Point(156, 414);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 34);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "사용 가능";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 20F);
+            this.label2.Location = new System.Drawing.Point(508, 414);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 34);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "사용 가능";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림", 15F);
+            this.label3.Location = new System.Drawing.Point(504, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(200, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "<오른쪽 세탁기>";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("굴림", 15F);
+            this.label4.Location = new System.Drawing.Point(177, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(175, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "<왼쪽 세탁기>";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 60000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("굴림", 15F);
+            this.label5.Location = new System.Drawing.Point(197, 469);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 25);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "45";
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("굴림", 15F);
+            this.label6.Location = new System.Drawing.Point(241, 469);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 25);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "00";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("굴림", 15F);
+            this.label7.Location = new System.Drawing.Point(228, 469);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(20, 25);
+            this.label7.TabIndex = 12;
+            this.label7.Text = ":";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("굴림", 15F);
+            this.label8.Location = new System.Drawing.Point(548, 469);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 25);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "45";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("굴림", 15F);
+            this.label9.Location = new System.Drawing.Point(581, 469);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(20, 25);
+            this.label9.TabIndex = 14;
+            this.label9.Text = ":";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("굴림", 15F);
+            this.label10.Location = new System.Drawing.Point(592, 469);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(38, 25);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "00";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("굴림", 15F);
+            this.button2.Location = new System.Drawing.Point(708, 431);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 58);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "start2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 60000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 1000;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // timer5
+            // 
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            // 
+            // timer6
+            // 
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(828, 518);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 메뉴ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 정보ToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.Timer timer6;
+    }
+}
+
---
+++ C# winform/example/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+using System.IO.Ports;    // 씨리얼 포트 사용하기 위해 필요
+using System.IO;
+
+namespace example
+{
+    public partial class Form1 : Form
+    {
+        StreamWriter sw = new StreamWriter("a.txt");
+        FileInfo file = new FileInfo("a.txt");
+        int duration1 = 59, duration2 = 59;
+        int duration = 44, duration3 = 44;
+        int stop1 = 0, stop3 = 0, stop4 = 0;
+        int stop2 = 0;
+        int temp = 1, temp1 = 1, state = 0;
+        string data_rx = "1\r";
+        string data_rx1 = "1\r";
+        private SerialPort ardSerialPort = new SerialPort();        // 사용할 씨리얼포트 객체 생성
+        public Form1()
+        {
+            InitializeComponent();
+            ardSerialPort.PortName = "COM3";
+            ardSerialPort.BaudRate = 9600;
+            ardSerialPort.Open();                                        // 포트 사용 준비 완료
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer5.Start();
+        }
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            duration--;
+            label5.Text = duration.ToString();
+            stop1++;
+            if(stop1 == 45)
+            {
+                duration = 44;
+                label5.Text = 45.ToString();
+                timer1.Stop();
+                stop1 = 0;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            duration1--;
+            
+            if (duration1 == -1)
+               {
+                  duration1 = 59;
+               }
+            label6.Text = duration1.ToString();
+            if(temp % 60 == 0)
+            {
+                stop2++;
+                if (stop2 == 45)
+                {
+                    label6.Text = "00";
+                    timer2.Stop();
+                    label1.Text = " 사용 가능";
+                    stop2 = 0;
+                    duration1 = 59;
+                    timer5.Start();
+                }
+            }
+            temp++;
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = " 사용 불가능";
+            timer3.Enabled = true;
+            timer3.Start();
+            timer4.Enabled = true;
+            timer4.Start();
+            label8.Text = duration3.ToString();
+            label10.Text = duration2.ToString();
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            duration3--;
+            label8.Text = duration3.ToString();
+            stop3++;
+            if (stop3 == 45)
+            {
+                duration3 = 44;
+                label8.Text = 45.ToString();
+                timer3.Stop();
+                stop3 = 0;
+            }
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            duration2--;
+
+            if (duration2 == -1)
+            {
+                duration2 = 59;
+            }
+            label10.Text = duration2.ToString();
+            if (temp1 % 60 == 0)
+            {
+                stop4++;
+                if (stop4 == 45)
+                {
+                    label10.Text = "00";
+                    timer4.Stop();
+                    label2.Text = " 사용 가능";
+                    stop4 = 0;
+                    duration2 = 59;
+                }
+            }
+            temp1++;
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader("b.txt");
+            int f1 = Int32.Parse(sr.ReadLine());
+            sr.Close();
+            data_rx = ardSerialPort.ReadLine();
+            sw.WriteLine(data_rx);
+            //textBox1.Text = data_rx;
+            if (data_rx == "0\r")
+            {
+                    timer5.Stop();
+                    if (timer1.Enabled == false && timer2.Enabled == false)
+                    {
+                        label1.Text = " 사용 불가능";
+                        timer1.Enabled = true;
+                        timer2.Enabled = true;
+                        timer1.Start();
+                        timer2.Start();
+                        sw.Close();
+                        label5.Text = duration.ToString();
+                        label6.Text = duration1.ToString();
+                        string imagePath = Application.StartupPath + "\\..\\..\\..\\..\\webserver\\";
+                        file.CopyTo(imagePath + "b" + f1 + ".txt");
+                        f1++;
+                        StreamWriter sw1 = new StreamWriter("b.txt");
+                        sw1.WriteLine(f1);
+                        sw1.Close();
+                        
+                    }
+            }
+        }
+
+        
+    }
+}
